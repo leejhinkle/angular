d7e1a46bec42eddb5992b8c100e4a6fb9d55079b
leejhinkle <leej@leejhinkle.org>
2015-09-08 15:12:43 -0600

    Partially through the creation of things for the weather-app

--- weather-app/js/app.js
+++ weather-app/js/app.js
@@ -1,4 +1,16 @@
 /**
  * Created by leejhinkle on 8/9/15.
  */
-var app = angular.module('weather-app',[]);
+var app = angular.module('weather-app',['ngRoute']);
+
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            controller: 'weatherController',
+            templateUrl: 'views/day.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
