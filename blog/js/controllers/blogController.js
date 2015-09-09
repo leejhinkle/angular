fad45924482144152154fa24d41ff2d9e42df249
leejhinkle <leej@leejhinkle.org>
2015-09-09 17:05:54 -0600

    never use firebase.set() commands.  THey suck.

--- blog/js/controllers/blogController.js
+++ blog/js/controllers/blogController.js
@@ -4,6 +4,11 @@
 
 //Be sure to inject all services and directives I might use
 app.controller('blogController',['$scope', function($scope){
+
+    //Set up the firebase server connection
+    $scope.firebaseConnection = new Firebase('https://ng-blog-app-hinkle.firebaseio.com/');
+
+    $scope.usersRef = $scope.firebaseConnection.child('users');
     $scope.createEntry = function(){
         alert("Create entry");
     };
