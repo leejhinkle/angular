f79a8dcb22b747f164123d3825a374ef8e0481c2
leejhinkle <leej@leejhinkle.org>
2015-09-09 15:26:15 -0600

    Got firebase working as long as I am online

--- blog/js/servers/server.js
+++ blog/js/servers/server.js
@@ -2,10 +2,13 @@
  * Created by leejhinkle on 9/9/15.
  */
 
-var Firebase = require('firebase');
-
 var firebaseConnection = new Firebase('https://ng-blog-app-hinkle.firebaseio.com/');
 
 firebaseConnection.set({
-    object1: "Hello World!"
+    object1: "Hello World!",
+    object2: "The ferrets are on fire",
+    post: {
+        author: "Nathan",
+        entry: "Chocolate microbes"
+    }
 });
