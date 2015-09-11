167941b3b04c19e9c99b5b1e6728a2e099a65adb
leejhinkle <leej@leejhinkle.org>
2015-09-11 16:37:25 -0600

    updating before changing computers

--- blog/js/controllers/blogController.js
+++ blog/js/controllers/blogController.js
@@ -3,15 +3,26 @@
  */
 
 //Be sure to inject all services and directives I might use
-app.controller('blogController',['$scope', function($scope){
+app.controller('blogController',['$scope', '$http', function($scope, $http){
 
     //Set up the firebase server connection
     $scope.firebaseConnection = new Firebase('https://ng-blog-app-hinkle.firebaseio.com/');
 
+    //read from server and get tot
+
     $scope.usersRef = $scope.firebaseConnection.child('users');
+    $scope.entryRef = $scope.firebaseConnection.child('entries');
+
+    //discover number of entries
     $scope.createEntry = function(){
-        alert("Create entry");
+
+
     };
+    $scope.submitEntry = function(entryIndex, entry){
+        //write to fB DB
+        $scope.entryRef.update({entryIndex : entry});
+    };
+
     $scope.getEntries = function(){
         alert("Get entries");
     };
