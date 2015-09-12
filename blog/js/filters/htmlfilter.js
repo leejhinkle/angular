e5ff7d20a25e55d94b8604e20c129554649a57c4
leejhinkle <leej@leejhinkle.org>
2015-09-11 19:54:57 -0600

    Final notes

--- blog/js/filters/htmlfilter.js
+++ blog/js/filters/htmlfilter.js
@@ -9,6 +9,12 @@ app.filter('unsafe', function($sce) {
 });
 
 /*
+ * This ties in with the filter on the blog/index.html page to take the strings stored in the firebase
+ * DB and output them as html, rather than as a string.  Apparently ng wants text input to be safe.
+ * I had to find a work-around.
+ * */
+
+/*
 *
 * Thanks to Creative Punch: http://creative-punch.net/2014/04/preserve-html-text-output-angularjs/
 * For the code assist
