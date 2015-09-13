d4273b975eb015385633d80b12bc7c9f7d9e987d
leejhinkle <leej@leejhinkle.org>
2015-09-13 17:10:07 -0600

    visual fun begins

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by leejhinkle on 13/9/15.
+ */
+var app = angular.module('portfolioApp',[]);
+
+app.controller('thisController', ['$scope', function($scope){
+    $scope.name = 'Nameless';
+}]);
